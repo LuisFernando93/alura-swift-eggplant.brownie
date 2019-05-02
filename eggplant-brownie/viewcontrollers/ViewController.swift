3ab5646c5715332a7c1d422048208335d0cd2593
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-05-02 17:50:58 -0300

    MealsTable -> delegate

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var nameField: UITextField?;
     @IBOutlet var happinessField: UITextField?;
-    var mealsTable: MealsTableViewController?
+    var delegate: MealsTableViewController?
     
     @IBAction func add() {
         
@@ -26,10 +26,10 @@ class ViewController: UIViewController {
             let meal = Meal(name: name, happiness: happiness)
             print("eaten \(meal.name) with happiness \(meal.happiness)!")
             
-            if (mealsTable == nil) {
+            if (delegate == nil) {
                 return
             }
-            mealsTable?.add(meal)
+            delegate?.add(meal)
             
             if let navigation = navigationController {
                 navigation.popViewController(animated: true)
