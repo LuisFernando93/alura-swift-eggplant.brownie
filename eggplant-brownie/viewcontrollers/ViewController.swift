d873c2e5abd194a81faffde4657d0c1c50b063b8
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-05-03 19:04:48 -0300

    Adicionado um TableView a segunda tela.

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet var nameField: UITextField?;
     @IBOutlet var happinessField: UITextField?;
     var delegate: AddAMealDelegate?
+    var items = [
+        Item(name: "Eggplant", calories: 10),
+        Item(name: "Brownie", calories: 10),
+        Item(name: "Zucchini", calories: 10),
+        Item(name: "Muffin", calories: 10),
+        Item(name: "Coconut oil", calories: 500),
+        Item(name: "Chocolate frosting", calories: 1000),
+        Item(name: "Chocolate chip", calories: 1000),
+    ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = item.name
+        return cell
+    }
     
     @IBAction func add() {
         
