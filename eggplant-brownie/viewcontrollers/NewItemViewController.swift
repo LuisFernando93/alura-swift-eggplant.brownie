04d4a3b92557fab9bad5f59afe21d9428f5e2313
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-05-04 16:08:03 -0300

    Nova tela e ViewController criados.

---
+++ eggplant-brownie/viewcontrollers/NewItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewItemViewController.swift
+//  eggplant-brownie
+//
+//  Created by Luis Fernando Pasquinelli on 04/05/19.
+//  Copyright © 2019 Luis. All rights reserved.
+//
+
+import UIKit
+
+class NewItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
