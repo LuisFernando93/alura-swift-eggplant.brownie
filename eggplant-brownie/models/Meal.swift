365b0216cfdc021e463e362fe2494e4bfa44f5ce
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-06-14 16:48:46 -0300

    Serializacao e desserializacao do Meal implementada.

--- eggplant-brownie/models/Meal.swift
+++ eggplant-brownie/models/Meal.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Meal {
+class Meal: NSCoding{
     let happiness: Int;
     let name: String;
     let items: Array<Item>
@@ -17,6 +17,12 @@ class Meal {
         self.name = name
         self.happiness = happiness
         self.items = items
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.happiness = aDecoder.decodeInteger(forKey: "happiness")
+        self.items = aDecoder.decodeObject(forKey: "items") as! Array<Item>
     }
     
     func allCalories() -> Double{
@@ -33,5 +39,11 @@ class Meal {
             message += "\n" + item.details()
         }
         return message
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(happiness, forKey: "happiness")
+        aCoder.encode(items, forKey: "items")
     }
 }
